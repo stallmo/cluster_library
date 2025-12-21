8ee6b4c249ad959d725dd6942f67b7e021a29947
Morris <32649574+stallmo@users.noreply.github.com>
2025-12-21 18:27:18 +0100

    Refactor initialization and membership handling
    Updated initialization methods and added checks for various initialization types. Improved handling of the membership matrix and convergence criteria.

--- fuzzy_cmeans.py
+++ fuzzy_cmeans.py
@@ -12,7 +12,7 @@ class FuzzyCMeans:
         self.__tol = tol  # epsilon
         self.__m = m
 
-        # going to be set when predict() is called
+        # going to be set when fit() is called
         self.__U = None  # assignment/ membership matrix
         self.__c = None  # cluster centers
         self.__iterations = 0
@@ -43,8 +43,34 @@ class FuzzyCMeans:
     def __init_centers(self, X, initialization):
 
         data_dim = X.shape[1]
+
+        if initialization == 'random_pick':
+            # Randomly choose points from X and choose as centers
+            indices = np.random.choice(X.shape[0], self.__num_clusters, replace=False)
+            self.__c = X[indices, :]
+
         if initialization == 'random':
-            self.__c = np.random.randint(low=400, high=700, size=((self.__num_clusters, data_dim)))
+            data_min = X.min()
+            data_max = X.max()
+            self.__c = np.random.uniform(low=data_min, high=data_max, size=((self.__num_clusters, data_dim)))
+
+        # implement kmeans ++ initialization
+        if initialization == 'k++':
+            # Choose first center randomly
+            self.__c = [X[np.random.choice(X.shape[0])]]
+
+            for _ in range(1, self.__num_clusters):
+                # Calculate minimal distance of every point to already chosen centers
+                dist_to_centers = distance.cdist(X, np.array(self.__c)).min(axis=1)
+
+                # Probability distribution based on distances
+                probabilities = dist_to_centers / dist_to_centers.sum()
+
+                # Choose next center based on probability distribution
+                next_center = X[np.random.choice(X.shape[0], p=probabilities)]
+                self.__c.append(next_center)
+
+            self.__c = np.array(self.__c)
 
         pass
 
@@ -71,6 +97,10 @@ class FuzzyCMeans:
         power = 2.0 / (self.__m - 1.0)
         dist_to_centers = distance.cdist(X, self.__c)
 
+        # Avoid division by zero
+        epsilon = 1e-10
+        dist_to_centers = np.maximum(dist_to_centers, epsilon)
+
         U_denom = np.zeros((X.shape[0], self.__num_clusters))
 
         # TODO: Optimize this bit
@@ -92,27 +122,33 @@ class FuzzyCMeans:
          Specifies how the initial membership assignment is to be performed. Only 'random' is implemented
         :return: None.
         """
-        if not initialization in ['random', 'federated']:
-            raise NotImplementedError
 
-        if initialization == 'federated':
-            assert self.__c is not None, 'For the federated initialization, the global server must first communicate the global cluster centers.'
-            self.__U = self.__calculate_cluster_membership(X)
+        if self.__iterations == 0:
 
-        if self.__U is None:
-            self.__init_centers(X, initialization)
-            self.__U = self.__calculate_cluster_membership(X)
-            # self.__init_membership(X, initialization)
+            if not initialization in ['random', 'random_pick', 'k++', 'federated']:
+                raise NotImplementedError
+
+            if initialization == 'federated':
+                assert self.__c is not None, 'For the federated initialization, the global server must first communicate the global cluster centers.'
+                print('Using federated initialization. Using global cluster centers for initialization.')
+
+            if self.__U is None:
+                self.__init_centers(X, initialization)
+                self.__U = self.__calculate_cluster_membership(X)
+                # self.__init_membership(X, initialization)
 
         for _iter in range(self.__max_iter):
 
             self.__iterations += 1
             U_prev = copy.deepcopy(self.__U)  # we'll need that to check termination criterion
-            self.__update_cluster_centers(X)
             # update membership matrix
             self.__U = self.__calculate_cluster_membership(X)
+            self.__update_cluster_centers(X)
             # check if we're done
-            if np.linalg.norm(self.__U - U_prev) < self.__tol:
+            diff = np.linalg.norm(self.__U - U_prev)
+            if diff < self.__tol and self.__iterations > 1:
+                print(
+                    f'Converged after {self.__iterations} iterations, because the change in membership matrix was {diff:.6f} < {self.__tol}.')
                 break
         pass
 
@@ -125,6 +161,6 @@ class FuzzyCMeans:
         """
 
         if self.__U is None:
-            print('Please call the fit() method first.')
+            raise RuntimeError('The model has not been fitted yet.')
 
         return self.__calculate_cluster_membership(X)
